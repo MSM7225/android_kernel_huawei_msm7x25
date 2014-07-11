4dd65f0e453bba070c5c991ad58ebc1e106b4cc2
Alberto96 <albertoguanti@gmail.com>
2014-07-11 13:58:46 +0200

    Revert "net: New iface_stat module to monitor persistent network stats"
    This reverts commit 21cbdc9965f17b23abdbcbfc7375da5fd1ea15c3.

--- net/core/dev.c
+++ net/core/dev.c
@@ -127,7 +127,6 @@
 #include <linux/jhash.h>
 #include <linux/random.h>
 #include <trace/events/napi.h>
-#include <linux/iface_stat.h>
 
 #include "net-sysfs.h"
 
@@ -5387,9 +5386,6 @@ int dev_change_net_namespace(struct net_device *dev, struct net *net, const char
 	unlist_netdevice(dev);
 
 	synchronize_net();
-
-	/* Store stats for this device in persistent iface_stat */
-	iface_stat_update(dev);
 
 	/* Shutdown queueing discipline. */
 	dev_shutdown(dev);
